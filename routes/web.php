5324bb74b5d49b4c0074f37b71109a87bfb71b7d
吴杰超 <215948874@qq.com>
2018-04-09 18:00:25 +0800

    最新修改
    最新修改

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,9 @@ Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 
 Route::put('/users/{user}', 'UsersController@update')->name('users.update');
 
+//话题页面
+
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+//话题页面分类
+Route::resource('categories','CategoriesController',['only'=>['show']]);
